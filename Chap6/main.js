8aa09150b944785b889233e80537edf8aaf3fa65
vivekchalana <vivekchalana@yahoo.com>
2018-11-08 12:12:15 -0800

    using js functions

--- Chap6/main.js
+++ Chap6/main.js
@@ -1,1 +1,25 @@
-alert("Hurrryayyyy!");
+let demo = document.getElementById("demo");
+
+let experience = Number(prompt("how many years of expereince you have?"));
+
+if(experience === 0){
+
+	demo.innerHTML = "you have no expereince"
+}
+if(experience === 1){
+
+	demo.innerHTML = "you are junior"
+}
+
+if(experience === 2){
+
+	demo.innerHTML = "you are intermediate"
+}
+if(experience === 3){
+
+	demo.innerHTML = "you are advanced"
+}
+if(experience === 4){
+
+	demo.innerHTML = "you are senior"
+}
