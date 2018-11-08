627854ab87fd5bf8b4cfb8ea7c2fba91c1083cda
vivekchalana <vivekchalana@yahoo.com>
2018-11-08 12:58:16 -0800

    more js scripts

--- Chap6/main.js
+++ Chap6/main.js
@@ -2,24 +2,63 @@ let demo = document.getElementById("demo");
 
 let experience = Number(prompt("how many years of expereince you have?"));
 
-if(experience === 0){
+// if(experience >=0 && experience <1){
 
-	demo.innerHTML = "you have no expereince"
-}
-if(experience === 1){
+// 	demo.innerHTML = "you have no expereince"
+// }
+// else if(experience >=1 && experience <2){
 
-	demo.innerHTML = "you are junior"
-}
+// 	demo.innerHTML = "you are junior"
+// }
 
-if(experience === 2){
+// else if(experience >=2 && experience <3){
 
-	demo.innerHTML = "you are intermediate"
-}
-if(experience === 3){
+// 	demo.innerHTML = "you are intermediate"
+// }
+// else if(experience >=3 && experience <4){
 
-	demo.innerHTML = "you are advanced"
-}
-if(experience === 4){
+// 	demo.innerHTML = "you are advanced"
+// }
+// else{
 
-	demo.innerHTML = "you are senior"
-}
+// 	demo.innerHTML = "you are senior"
+// }
+
+// switch (experience) {
+
+// 	case 0:
+// 		demo.innerHTML= "you have no experience";
+// 		break;
+// 	case 1:
+// 		demo.innerHTML= "you are junior";
+// 		break;
+// 	case 2:
+// 		demo.innerHTML= "you are intermediate";
+// 		break;
+// 	case 3:	
+// 		demo.innerHTML= "you are advanced";
+// 		break;
+// 	default:
+// 		demo.innerHTML= "you are senior";
+// 		break;
+
+// 	}
+
+switch (true) {
+
+	case experience <0:
+		demo.innerHTML= "you have no experience";
+		break;
+	case experience <=1:
+		demo.innerHTML= "you are junior";
+		break;
+	case experience <=2:
+		demo.innerHTML= "you are intermediate";
+		break;
+	case experience <=3:	
+		demo.innerHTML= "you are advanced";
+		break;
+	default:
+		demo.innerHTML= "you are senior";
+		break;
+		}
