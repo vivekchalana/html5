c79d9855c0fb4f437cc391030fbe6f0250475516
vivekchalana <vivekchalana@yahoo.com>
2018-11-09 12:41:16 -0800

    onblur in java script

---
+++ Chapter8/js/main.js
@@ -0,0 +1,8 @@
+function myfunction(){
+
+	document.getElementsByClassName("formInputs")[0].style.background = "lightgray"
+}
+function myfunction2(){
+
+	document.getElementsByClassName("formInputs")[1].style.background = "lightgray"
+}
