59bec0c8ded7f11c6d288fcc6db7c3b2c881165b
vivekchalana <vivekchalana@yahoo.com>
2018-11-13 09:50:44 -0800

    using let and ID here

--- Chapter9-Javascript array/js/main.js
+++ Chapter9-Javascript array/js/main.js
@@ -1,21 +1,29 @@
-// // let student = ["leo", "mack", "john", "David"];
+// // // let student = ["leo", "mack", "john", "David"];
 
-// // document.getElementById("Demo").innerHTML = student[2];
+// // // document.getElementById("Demo").innerHTML = student[2];
 
-// let userDetails = [];
+// // let userDetails = [];
 
-// userDetails[0] = prompt("What is your name");
-// userDetails[0] = prompt("What is last name");
-// userDetails[0] = prompt("How old are you");
+// // userDetails[0] = prompt("What is your name");
+// // userDetails[0] = prompt("What is last name");
+// // userDetails[0] = prompt("How old are you");
 
-// document.getElementById("Demo").innerHTML = userDetails[0]+""+userDetails[1] + "" + userDetails[2]
+// // document.getElementById("Demo").innerHTML = userDetails[0]+""+userDetails[1] + "" + userDetails[2]
 
-function myFunction(event){
-          event.preventDefault();
-         let name = document.getElementById('nameinput').value;
-         let lastName = document.getElementById('lastNameInput').value;
-         let emailInput = document.getElementById('emailInput').value;
+// function myFunction(event){
+//           event.preventDefault();
+//          let name = document.getElementById('nameinput').value;
+//          let lastName = document.getElementById('lastNameInput').value;
+//          let emailInput = document.getElementById('emailInput').value;
 
 
-         document.getElementById('inputSummary').innerHTML = `Hello ${name} ${lastName}, thank you for filling out and submitting the form. You will receive an email to the provided email: <h4>${emailInput}</h4>`;
-}
+//          document.getElementById('inputSummary').innerHTML = `Hello ${name} ${lastName}, thank you for filling out and submitting the form. You will receive an email to the provided email: <h4>${emailInput}</h4>`;
+// }
+
+let contactInfo = [];
+
+	contactInfo[0] = "Vivek Chalana"
+	contactInfo[1] = "35"
+	contactInfo[2] = "True"
+
+	document.getElementById("here").innerHTML = contactInfo;
