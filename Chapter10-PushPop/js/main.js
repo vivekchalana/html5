3cfba6e500375b43f253dfd5c1bc7190367efb6d
vivekchalana <vivekchalana@yahoo.com>
2018-11-13 12:12:21 -0800

    multiple arrays

---
+++ Chapter10-PushPop/js/main.js
@@ -0,0 +1,6 @@
+let farm = ["horse", "sheep", "goat", "donkey", "horse"];
+let pets = ["cat", "dog", "fish"];
+let rodents = ["hamster", "genipig", "rabbit", "squirrel"];
+
+let allAnimals = [...farm,...pets,...rodents];
+console.log(allAnimals);
