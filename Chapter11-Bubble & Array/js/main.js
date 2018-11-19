d9af8a445425bb11bee98278fc632487fcf6acd9
vivekchalana <vivekchalana@yahoo.com>
2018-11-19 12:48:52 -0800

    creating object

--- Chapter11-Bubble & Array/js/main.js
+++ Chapter11-Bubble & Array/js/main.js
@@ -18,23 +18,44 @@
 
 // }
 
-let aDombObj = document.getElementById('test');
-aDombObj.addEventListener('dblclick', startCounting);
+// let aDombObj = document.getElementById('test');
+// aDombObj.addEventListener('dblclick', startCounting);
 
-let t = 0;
-let myVariable = setInterval(function(){
+// let t = 0;
+// let myVariable = setInterval(function(){
 
-	startCounting();
-}, 1000);
+// 	startCounting();
+// }, 1000);
 
-function startCounting() {
+// function startCounting() {
 
-			document.getElementById("test").innerHTML = t+=1;
-		if(t===10){
-			clearInterval();
+// 			document.getElementById("test").innerHTML = t+=1;
+// 		if(t===10){
+// 			clearInterval();
+// 		}
+
+// 	}
+
+class person{
+
+	constructor(name,canDance){
+		this.name = name;
+		this.canDance = canDance;
+		this.speak = function(){
+
+			console.log(`hello my name is ${this.name} and people say i can dance thats ${this.canDance}`);
 		}
 
 	}
+}
+
+let vivekObj = new person("Vivek", true);
+let danielObj = new person("Daniel", false);
+let davidObj = new person("David", true);
+
+	vivekObj.speak();
+	danielObj.speak();
+	davidObj.speak();
 
 
 
