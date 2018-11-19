75a7eb587a0890545b1a2f8a1a8dd05319d4bd1a
vivekchalana <vivekchalana@yahoo.com>
2018-11-19 09:22:52 -0800

    setting and clear intervals

--- Chapter11-Bubble & Array/js/main.js
+++ Chapter11-Bubble & Array/js/main.js
@@ -1,10 +1,42 @@
+// let aDombObj = document.getElementById('test');
+// aDombObj.addEventListener('dblclick', addSomeText);
+
+// function addSomeText() {
+
+// 	setTimeout(function(){
+// 		aDombObj.innerHTML = "hello Javascript"; }, 4000);
+
+// }
+
+// let aDombObj = document.getElementById('test');
+// aDombObj.addEventListener('dblclick', addSomeText);
+
+// function addSomeText() {
+
+// 	setInterval(function(){
+// 		console.log("set Interval function")}, 4000);
+
+// }
+
 let aDombObj = document.getElementById('test');
-aDombObj.addEventListener('dblclick', addSomeText);
+aDombObj.addEventListener('dblclick', startCounting);
 
-function addSomeText() {
+let t = 0;
+let myVariable = setInterval(function(){
 
-	setTimeout(function(){
-		aDombObj.innerHTML = "hello Javascript"; }, 4000);
+	startCounting();
+}, 1000);
 
-}
+function startCounting() {
+
+			document.getElementById("test").innerHTML = t+=1;
+		if(t===10){
+			clearInterval();
+		}
+
+	}
+
+
+
+
 
